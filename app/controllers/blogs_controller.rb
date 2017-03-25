679af3ef0098afb2a9de6a77a72c8644ec3f8ab7
morgan <rabrosimov@gmail.com>
2017-03-26 00:37:51 +0200

    portfolio

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -61,6 +61,12 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    @blog = Blog.friendly.find(params[:id])
+    @blog.published? ? @blog.draft! : @blog.published!
+    redirect_to blogs_path, notice: 'Post status has been updated'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :portfolios
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   get 'about',   to: 'pages#about'
   get 'contact', to: 'pages#contact'
