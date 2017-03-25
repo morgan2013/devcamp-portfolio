1b48972e98bea1364a92d27dbe319424db7d920a
morgan <rabrosimov@gmail.com>
2017-03-25 21:03:47 +0200

    portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'about',   to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-
+  root to: 'pages#home'
   resources :portfolios
   resources :blogs
 
-  root to: 'pages#home'
+  get 'about',   to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
