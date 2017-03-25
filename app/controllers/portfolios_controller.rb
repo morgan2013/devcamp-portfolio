b18c35e92bf114a3c0c62ee31760398218aaa660
morgan <rabrosimov@gmail.com>
2017-03-25 20:32:53 +0200

    portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,6 +40,15 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio = Portfolio.find(params[:id]);
+    @portfolio.destroy
+    respond_to do |format|
+      format.html{ redirect_to portfolios_path, notice: 'Portfolio was removed'}
+      format.json{ head :no_content }
+    end
+  end
+
   private
 
   def portfolio_params
