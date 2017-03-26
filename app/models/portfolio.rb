cc0fbde89ba2bc4587d06b42e3092e44ea38d2fc
morgan <rabrosimov@gmail.com>
2017-03-26 17:50:35 +0300

    portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,15 @@
 class Portfolio < ApplicationRecord
+  validates :title, :body, :main_image, :thumb_image, presence: true
 
   scope :angular, -> { where(subtitle: 'Angular').order(created_at: :desc) }
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails').order(created_at: :desc) }
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.main_image ||= 'http://placehold.it/600x400'
+    self.thumb_image ||= 'http://placehold.it/350x200'
+  end
 end
