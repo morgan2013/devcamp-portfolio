aa28c99f4f2d257bd93693d5cd8d6c30710a9e82
morgan <rabrosimov@gmail.com>
2017-03-26 18:17:15 +0300

    portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates :title, :body, :main_image, :thumb_image, presence: true
 
   scope :angular, -> { where(subtitle: 'Angular').order(created_at: :desc) }
@@ -9,7 +10,7 @@ class Portfolio < ApplicationRecord
   private
 
   def set_defaults
-    self.main_image ||= 'http://placehold.it/600x400'
-    self.thumb_image ||= 'http://placehold.it/350x200'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
