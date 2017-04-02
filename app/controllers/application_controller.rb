a7ea236716861bde23db3c1babf8141576979c86
morgan <rabrosimov@gmail.com>
2017-04-02 21:35:34 +0300

    install devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+
+  def current_user
+    super || OpenStruct.new(name: 'Guest',
+                            first_name: 'guest',
+                            last_name: 'guest',
+                            email: 'example.com')
+  end
 end
